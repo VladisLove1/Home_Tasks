256f5909b9088e78da90bf7b438fa9f071ecccd4
Vlad Khymenko <vladislove0212@gmail.com>
2017-07-07 22:13:30 +0300

    not even a half :(

---
+++ functions_forms_tasks/1/1.php
@@ -0,0 +1,20 @@
+<?php
+require_once '1.html';
+$a = $_POST ['a'];
+$b = $_POST ['b'];
+function getCommonWords($a, $b){
+$a = explode(' ',$a);
+$b = explode(' ',$b);
+for ($i = 0; $i < count($a); $i++) {
+$test = false;
+for ($j = 0; $j < count($b); $j++) {
+if($a[$i] === $b[$j]){
+$result[]=$a[$i];
+$test = true;
+break;
+}
+}if($test == false){
+}
+}return $result;
+}
+print_r(getCommonWords($a,$b));
---
+++ functions_forms_tasks/2/2.php
@@ -0,0 +1,19 @@
+<?php
+require_once '2.html';
+$a = $_POST['a'];
+//$a = 'qwerrrr 123456789 qwert qwerty';
+function leght_top_three($a)
+{
+    $a = explode(' ', $a);
+    for ($i = 0; $i < count($a); $i++) {
+        for ($j = 0; $j < count($a)-1; $j++) {
+            if (mb_strlen($a[$j]) > mb_strlen($a[$j+1])){
+                $c=$a[$j];
+                $a[$j]=$a[$j+1];
+                $a[$j+1]=$c;
+            }
+        }
+    }
+    return (array_slice($a, 0, 3));
+}
+print_r(leght_top_three($a));
