273b84099ffd7835ca2d54ab3a3372d3ce5a21e7
Vlad Khymenko <vladislove0212@gmail.com>
2017-08-07 15:56:31 +0300

    objects

---
+++ LESSON_7/LESSON_7.js
@@ -0,0 +1,34 @@
+var str = "January,February,March,April,May,June,July,August,September,October,November,December";
+/*var arr_str = str.split(',');
+console.log(arr_str[1]);*/
+
+var winter = {};
+var spring = {};
+var summer = {};
+var autumn = {};
+
+winter.december = {"number": '12', "days": '31'};
+winter.january = {"number": '01', "days": '31'};
+winter.february = {"number": '02', "days": '28'};
+
+spring.march = {"number": '03', "days": '31'};
+spring.april = {"number": '04', "days": '30'};
+spring.may = {"number": '05', "days": '31'};
+
+summer.june = {"number": '06', "days": '30'};
+summer.july = {"number": '07', "days": '31'};
+summer.august = {"number": '08', "days": '31'};
+
+autumn.september = {"number": '09', "days": '30'};
+autumn.october = {"number": '10', "days": '31'};
+autumn.november = {"number": '11', "days": '30'};
+
+/*
+console.log(winter);
+console.log(spring);
+console.log(summer);
+console.log(autumn);
+*/
+
+
+
