3cea55eae67030b9967ffbe2684a244aa9fda006
Vlad <vkhymenkov@gmail.com>
2017-09-04 18:45:09 +0300

    added validation on start

--- LESSON_11/LESSON_11.js
+++ LESSON_11/LESSON_11.js
@@ -4,6 +4,7 @@ function Components() {
 
     this.active = function () {
         active_or_not = true;
+        newForm.validate();
     };
     this.disabled = function () {
         active_or_not = false;
@@ -38,18 +39,18 @@ function Form() {
 var newForm = new Form, newComp = new Components;
 
 //newComp.active();
-newComp.disabled();
+//newComp.disabled();
 console.log(newComp.getActive_Or_Not() + " // here we check if our Component is active (true) or disabled (false)");
 
 var is_active = newComp.getActive_Or_Not();
 
 if (is_active !== false && is_active !== undefined) {
-    console.log("//and if it is TRUE, then we have our 'validation' and 'send' methods ran, like you see below");
+    console.log("//and if it is TRUE, then we have our 'validation' and 'send' methods available, like you see below");
     newForm.validate();
     newForm.send();
 }
 else {
-    console.log("And if it is FALSE, we'll see this message" + " 'Can not be sent or validated until class Components is disabled'");
+    console.log("And if it is FALSE or UNDEFINED, we'll see this message" + " 'Can not be sent or validated until class Components is active'");
 }
 
 /*newForm.setStatus(11);
