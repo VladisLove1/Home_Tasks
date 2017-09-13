239ad8371315c6146803ef73e40c9129b08210e3
Vlad <vkhymenkov@gmail.com>
2017-09-13 23:54:06 +0300

    used appendChild, but still not sure

--- LESSON_12/lesson_12.js
+++ LESSON_12/lesson_12.js
@@ -1,36 +1,33 @@
-/*Домашнее задание № 12
-На странице есть список, такого вида:
-    <ul id="list">
-    <li>Element 1</li>
-<li>Element 2</li>
-<li style="color: green">Element 3</li>
-<li style="color: green">Element 4</li>
-<li>Element 5</li>
-<li style="color: green">Element 6</li>
-<li style="color: green">Element 7</li>
-<li style="color: green">Element 8</li>
-<li>Element 9</li>
-<li style="color: green">Element 10</li>
-</ul>
+var elems = document.getElementsByTagName('li'); // getting array of <li> elements
 
-Задача:
-В каждый элемент списка зеленого цвета добавить тег SPAN, а если элемент черного цвета, добавить тег Р. Внутренний текст должен сохранится.
-    Пример,
-<li style="color: green"><span>Element 10</span></li>
-или
-<li><p>Element 1</p></li>*/
-var list = document.getElementById('list');
-
-var elems = document.getElementsByTagName('li'); //creating array of 'li' elements
-
-// adding <p> or <span> when needed with saving default HTML
-for (var i = 0; i < elems.length; i++){
-    if (elems[i].style.color === 'green'){
-        elems[i].innerHTML += "<br><span> SPAN tag added </span><br>";
+for (var i = 0; i <= elems.length; i++ ) {
+    //debugger;
+    var p = document.createElement('p'); // assigning creation of <p> to var p
+    var span = document.createElement('span'); // assigning creation of <span> to var span
+    var color = elems[i].style.color; // getting color of the 'i' element and assigning it to var color
+    var data = elems[i].childNodes[0].data; // getting data (text) of 'i' element and assigning it to var data
+    if (color !== 'green' && color !== undefined) { // checking
+        elems[i].innerHTML = ""; // removing inner elements of 'i' element
+        p.innerHTML = data; // adding our data into <p>
+        elems[i].appendChild(p); // appending added data to 'i' element
     }
-    else {
-        elems[i].innerHTML += "<p> P tag added</p>";
+    if (color === 'green') { // another check
+        elems[i].innerHTML = ""; // all the same, just <span> instead of <p>
+        span.innerHTML = data;
+        elems[i].appendChild(span);
     }
 }
+console.log(elems[0]);
 
-console.log(list);
+/* var list = document.getElementById('list');
+
+ var elems = document.getElementsByTagName('li'); //creating array of 'li' elements
+
+for (var i = 0; i < elems.length; i++){
+    if (elems[i].style.color === 'green'){
+        elems[i].innerHTML = "<br><span>" + elems[i].childNodes[0].data + "</span><br>";
+    }
+    else {
+        elems[i].innerHTML = "<p>" + elems[i].childNodes[0].data + "</p>";
+    }
+}*/
